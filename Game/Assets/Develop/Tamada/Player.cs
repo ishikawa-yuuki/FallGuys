14d2e4e9a67bf7f80d5d14cc4861bcf0a45f7d7d
Yuuki Ishikawa <kbc18b01@stu.kawahara.ac.jp>
2020-10-01 13:49:51 +0900

    苦しい

---
+++ Game/Assets/Develop/Tamada/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 speed;//プレイヤーが所持する移動ベクトル。
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        //清算
+        this.transform.position += speed;
+        //リセット
+        speed =new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    //移動系
+    public void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed.x += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            speed.x -= 0.1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            speed.z -= 0.1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            speed.z += 0.1f;
+        }
+
+
+    }
+
+}
---
+++ Game/Assets/Develop/Tamada/Tamacamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tamacamera : MonoBehaviour
+{
+    //private GameObject player;   //プレイヤー情報格納用
+    public Transform target;    //追従するターゲット。
+    public Vector3 offset;  //プレイヤーとカメラの距離。
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.player = GameObject.Find("Player1");
+
+        offset = new Vector3(-10.0f, 3.0f, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //カメラ向き
+        //transform.rotation = player.transform.rotation;
+        transform.position = target.position + offset;
+        this.transform.LookAt(target.position);
+
+    }
+}
